d0d9fd16c19f51b2fdae5d24e1396908f69eafc9
YeongJun Kim <yeongjun0925@gmail.com>
2022-11-29 17:51:06 +0900

    제품 추가 기능 구현

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Category;
+use App\Models\Product;
 
 class AdminController extends Controller
 {
@@ -29,5 +30,34 @@ class AdminController extends Controller
         $data->delete();
 
         return redirect()->back()->with('message', 'Category Deleted Successfully');
+    }
+
+    public function view_product()
+    {
+        $category = Category::all();
+        return view('admin.product', compact('category'));
+    }
+
+    public function add_product(Request $request)
+    {
+        $product = new Product;
+        $product->title = $request->title;
+        $product->description = $request->description;
+        $product->price = $request->price;
+        $product->quantity = $request->quantity;
+        $product->discount_price = $request->dis_price;
+        $product->category = $request->category;
+        
+        // 파일 처리
+        $image = $request->image;
+        // 이미지에 고유한 값을 부여하기 위해
+        $imagename = time().'.'.$image->getClientOriginalExtension();
+        // public > product
+        $request->image->move('product', $imagename);
+        $product->image = $imagename;
+
+        $product->save();
+
+        return redirect()->back()->with('message', 'Product Added Successfully');
     }
 }
