6e2d98f2d81b0d2b37f3859c1924b4591c7f495b
YeongJun Kim <yeongjun0925@gmail.com>
2022-12-01 09:44:52 +0900

    장바구니 정보 주문 테이블로 넘기기

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Auth;
 use App\Models\User;
 use App\Models\Product;
 use App\Models\Cart;
+use App\Models\Order;
 
 class HomeController extends Controller
 {
@@ -88,5 +89,40 @@ class HomeController extends Controller
         $cart->delete();
 
         return redirect()->back();
+    }
+
+    public function cash_order()
+    {
+        $user = Auth::user();
+        $userid = $user->id;
+
+        $data = Cart::where('user_id', '=', $userid)->get();
+        
+        foreach($data as $data) {
+            $order = new Order;
+            $order->name = $data->name;
+            $order->email = $data->email;
+            $order->phone = $data->phone;
+            $order->address = $data->address;
+            $order->user_id = $data->user_id;
+
+            $order->product_title = $data->product_title;
+            $order->price = $data->price;
+            $order->quantity = $data->quantity;
+            $order->image = $data->image;
+            $order->product_id = $data->product_id;
+
+            $order->payment_status = 'cash on delivery';
+            $order->delivery_status = 'processing';
+
+            $order->save();
+
+            // Cart에서 Order로 이동했으므로 Cart 데이터 삭제
+            $cartid = $data->id;
+            $cart = Cart::find($cartid);
+            $cart->delete();
+        }
+
+        return redirect()->back()->with('message', 'We Received Your Order. We will connect with you soon...');
     }
 }
--- resources/views/home/showcart.blade.php
+++ resources/views/home/showcart.blade.php
@@ -49,7 +49,14 @@
         <div class="hero_area">
             <!-- header section strats -->
             @include('home.header')
-                <!-- end header section -->
+            <!-- end header section -->
+
+            @if (session()->has('message'))
+                <div class="alert alert-success">
+                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">x</button>
+                    {{ session()->get('message') }}
+                </div>
+            @endif
 
             <div class="center">
                 <table>
@@ -74,6 +81,12 @@
 
                 <div>
                     <h1 class="total_deg">Total Price: ${{ $totalPrice }}</h1>
+                </div>
+
+                <div>
+                    <h1 style="font-size: 25px; padding-bottom: 15px;">Proceed to Order</h1>
+                    <a href="{{ url('cash_order') }}" class="btn btn-danger">Cash on Delivery</a>
+                    <a href="" class="btn btn-danger">Pay Using Card</a>
                 </div>
             </div>
 
